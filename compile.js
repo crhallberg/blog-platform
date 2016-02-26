c0dd89d49a25774a7214a1b6e354675a90ee78ad
Chris Hallberg <crhallberg@gmail.com>
2016-02-26 13:07:18 -0500

    HTML5 DOCTYPE.

--- compile.js
+++ compile.js
@@ -32,7 +32,7 @@ fs.readdir('./posts', function(err, files) {
     console.log(' - compiled');
     var name = 'Chris Hallberg';
     var header = '\
-<html><head>\
+<!DOCTYPE html><html><head>\
 <meta charset="utf-8">\
 <meta http-equiv="X-UA-Compatible" content="IE=edge">\
 <meta name="viewport" content="width=device-width, initial-scale=1">\
